46a4261a4c0f2ff88d023ed38a0b8bd88aa0c1fd
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-05-02 03:08:22 -0700

    Add ConvertIntegerToString.java

---
+++ src/main/java/challenge/ConvertIntegerToString.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package challenge;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+
+//Problem Link: https://www.hackerrank.com/challenges/java-int-to-string/problem?isFullScreen=true
+
+public class ConvertIntegerToString {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = String.valueOf(n);
+        System.out.println(s);
+    }
+    
+}
