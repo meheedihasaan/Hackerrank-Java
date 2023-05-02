a911d210a39a28d4c1a89b576540496fa0bcbf33
Mehedi Hasan <mehedihasan111941@gmail.com>
2023-05-02 01:59:58 -0700

    Initial Commit

---
+++ src/main/java/challenge/WelcomeToJava.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package challenge;
+
+/**
+ *
+ * @author User
+ */
+
+//Problem Link: https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+
+public class WelcomeToJava {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+    
+}
